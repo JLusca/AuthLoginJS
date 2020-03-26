52a00904c1ee6b5a3ca91ad8bcd90357f18ed551
JLusca <62689072+JLusca@users.noreply.github.com>
2020-03-26 04:02:02 -0300

    Upload do código do sistema de automatização utilizando Javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function Logar(){
+
+ 
+ var Login = document.getElementById("Login").value;
+ 
+ var Senha = document.getElementById("Senha").value;
+     //IF compara
+if (Login == "Thiago" && Senha == "123") {alert("Você Logou Seu Filho Da Puta");}
+else {alert("Voce Falhou Miseravelmente");}
+	
+	}
+	//pegar a entrada primeiro}
+    //Todo comando é executado dentro das chaves
